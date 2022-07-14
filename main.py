be79f99cd98cd5d290c45a4be605856481bb3ef5
unknown <pandupurwadi18@gmail.com>
2022-07-14 21:30:34 +0700

    update

--- main.py
+++ main.py
@@ -12,15 +12,21 @@ nr_letters= int(input("How many letters would you like in your password?\n"))
 nr_symbols = int(input(f"How many symbols would you like?\n"))
 nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-password = ""
+password_list = []
 
 for char in range (1, nr_letters+1):
-    password += random.choice(letters)
+    password_list.append(random.choice(letters))
 
 for char in range (1, nr_symbols+1):
-    password += random.choice(symbols)
+    password_list.append(random.choice(symbols))
 
-for char in range (1, nr_numbers):
-    password += random.choice(numbers)
+for char in range (1, nr_numbers+1):
+    password_list.append(random.choice(numbers))
 
-print(password)
+random.shuffle(password_list)
+
+password = ""
+for char in password_list:
+    password += char
+
+print(f"Your Password is : {password}")
